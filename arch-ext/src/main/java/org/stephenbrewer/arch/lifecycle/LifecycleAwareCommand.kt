da37eedaed3d1d78cc0a0d662e50019ca1ee1bcd
Stephen Brewer <git@stephenbrewer.org>
2019-10-12 16:56:04 +0300

    added arch-ext

---
+++ arch-ext/src/main/java/org/stephenbrewer/arch/lifecycle/LifecycleAwareCommand.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 Stephen Brewer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.stephenbrewer.arch.lifecycle
+
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.annotation.MainThread
+
+class LifecycleAwareCommand<T> {
+
+    private val observers = ArrayList<CommandLifecycleObserver>(1)
+
+    @MainThread
+    fun observe(lifecycleOwner: LifecycleOwner, code: (parameter: T?) -> Unit) {
+        val singleEventObserver = CommandLifecycleObserver(lifecycleOwner, code)
+        lifecycleOwner.lifecycle.addObserver(singleEventObserver)
+        observers.add(singleEventObserver)
+    }
+
+    // DataBinding does not recognise `execute(parameter: T? = null)` when using the default value
+    // so defining this
+    @MainThread
+    fun execute() {
+        execute(null)
+    }
+
+    @MainThread
+    fun execute(parameter: T?) {
+        for (it in observers) {
+            it.executeIfActive(parameter)
+        }
+    }
+
+    private inner class CommandLifecycleObserver(
+        private val lifecycleOwner: LifecycleOwner,
+        private val code: (parameter: T?) -> Unit
+    ) : DefaultLifecycleObserver {
+
+        private fun isActive() = lifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
+
+        fun executeIfActive(parameter: T?) {
+            if (isActive()) {
+                code.invoke(parameter)
+            }
+        }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            super.onDestroy(owner)
+            observers.remove(this@CommandLifecycleObserver)
+            owner.lifecycle.removeObserver(this@CommandLifecycleObserver)
+        }
+    }
+}
---
+++ arch-ext/src/main/java/org/stephenbrewer/arch/livedata/Transformations.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2019 Stephen Brewer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.stephenbrewer.arch.livedata
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+
+/**
+ * LiveData that only triggers updates when the change different from the previous value
+ */
+@MainThread
+fun <T> LiveData<T?>.distinct(): LiveData<T?> {
+    val distinctLD = MediatorLiveData<T?>()
+    distinctLD.addSource(this, object : Observer<T?> {
+        private var previous: T? = null
+
+        override fun onChanged(it: T?) {
+            if (it != previous) {
+                previous = it
+                distinctLD.value = it
+            }
+        }
+    })
+
+    return distinctLD
+}
+
+/**
+ * LiveData that only triggers updates when the change is non null
+ */
+@MainThread
+fun <T> LiveData<T?>.nonNull(): LiveData<T> {
+    val nonNullLD = MediatorLiveData<T>()
+    nonNullLD.addSource(this) {
+        if (it != null) {
+            nonNullLD.value = it
+        }
+    }
+
+    return nonNullLD
+}
+
+/**
+ * LiveData that emits log statements
+ */
+@MainThread
+fun <T> LiveData<T?>.log(logTag: String): LiveData<T?> {
+    val loggingLD = LoggingLiveData<T?>(logTag)
+    loggingLD.addSource(this) {
+        Log.d(logTag, "onChanged       : [$it].")
+        loggingLD.value = it
+    }
+
+    return loggingLD
+}
+
+/**
+ * LiveData that emits log statements
+ */
+class LoggingLiveData<T>(private val logTag: String): MediatorLiveData<T>() {
+    private var observerCounter = 0
+
+    override fun onActive() {
+        Log.d(logTag, "onActive        : [${this.value}]")
+        super.onActive()
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        Log.d(logTag, "onInactive      : [${this.value}]")
+    }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        observerCounter++
+        Log.d(logTag, "observe         : [$observerCounter] [${owner.lifecycle.currentState}]")
+        super.observe(owner, observer)
+    }
+
+    override fun observeForever(observer: Observer<in T>) {
+        observerCounter++
+        Log.d(logTag, "observeForever  : [$observerCounter]")
+        super.observeForever(observer)
+    }
+
+    override fun removeObserver(observer: Observer<in T>) {
+        super.removeObserver(observer)
+        observerCounter--
+        Log.d(logTag, "removeObserver  : [$observerCounter]")
+    }
+}
