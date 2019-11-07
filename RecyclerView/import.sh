98ba966335240ff9bfb9ad1f3c92721d42f1782e
Stephen Brewer <git@stephenbrewer.org>
2019-11-07 12:56:53 +0300

    fix broken import

--- RecyclerView/import.sh
+++ RecyclerView/import.sh
@@ -2,20 +2,21 @@
 
 SRC_DIR=$HOME/Development/androidx-master-dev/frameworks/support/recyclerview/recyclerview
 SRC_DIR_CODE=$SRC_DIR/src/main/java
-SRC_DIR_RES=$SRC_DIR/res/values
+SRC_DIR_RES=$SRC_DIR/res
 DST_DIR=$HOME/Development/Android/CommonCode/RecyclerView/src/main
 DST_DIR_CODE=$DST_DIR/java
-DST_DIR_RES=$DST_DIR/res/values
+DST_DIR_RES=$DST_DIR/res
 SRC_PKG_DIR=androidx/recyclerview/widget
 DST_PKG_DIR=org/stephenbrewer/arch/recyclerview
 SRC_PKG=androidx.recyclerview.widget
 DST_PKG=org.stephenbrewer.arch.recyclerview
 
-rm -r $DST_DIR
+rm -r $DST_DIR_CODE
+rm -r $DST_DIR_RES
 mkdir -p $DST_DIR_CODE/$DST_PKG_DIR
 mkdir -p $DST_DIR_RES
 cp -r $SRC_DIR_CODE/$SRC_PKG_DIR/* $DST_DIR_CODE/$DST_PKG_DIR
-cp $SRC_DIR_RES/* $DST_DIR_RES
+cp -r $SRC_DIR_RES/* $DST_DIR_RES
 
 cd $DST_DIR_CODE/$DST_PKG_DIR
 sed -i '' -e 's/package androidx.recyclerview.widget/package org.stephenbrewer.arch.recyclerview/g' *.*
